62d99101ec68d78f7998b35dc425aedd8144e98c
Alvaro Clemente <clemente.varo@gmail.com>
2024-07-25 00:04:32 +0200

    feat: block statements

--- grammar.js
+++ grammar.js
@@ -3,14 +3,14 @@ module.exports = grammar({
   rules: {
     object: $ => repeat1(choice($.statement, $.comment)),
     key: $ => $._identifier,
-    // TODO: Figure out how to do line comments
+    // TODO: Figure out how to do rest of line comments
     comment: $ => seq('#', /[^\n]*/),
     statement: $ => seq($.key, ':', $._value),
-    _identifier: $ => /[a-zA-Z_][a-zA-Z0-9-_]+/,
-    _value: $ => choice($.bool, $.number, $.string),
     bool: $ => choice('yes', 'no', 'true', 'false'),
     number: $ => choice(/[0-9]+/, /[0-9]*\.[0-9]+/),
     string: $ => seq('"', token.immediate(/[^"]+/), token.immediate('"')),
-    //_block: $ =>
+    block: $ => seq('{', $.object, '}'),
+    _identifier: $ => /[a-zA-Z_][a-zA-Z0-9-_]+/,
+    _value: $ => choice($.bool, $.number, $.string, $.block),
   }
 })
