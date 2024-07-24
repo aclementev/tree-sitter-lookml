fc1ee5332c15de930bb2eecb69c4b07644a5a8e6
Alvaro Clemente <clemente.varo@gmail.com>
2024-07-25 00:13:11 +0200

    feat: add named blocks

--- grammar.js
+++ grammar.js
@@ -9,8 +9,10 @@ module.exports = grammar({
     bool: $ => choice('yes', 'no', 'true', 'false'),
     number: $ => choice(/[0-9]+/, /[0-9]*\.[0-9]+/),
     string: $ => seq('"', token.immediate(/[^"]+/), token.immediate('"')),
-    block: $ => seq('{', $.object, '}'),
+    block: $ => $._block,
+    _block: $ => seq('{', $.object, '}'),
+    named_block: $ => seq(field('name', $._identifier), $._block),
     _identifier: $ => /[a-zA-Z_][a-zA-Z0-9-_]+/,
-    _value: $ => choice($.bool, $.number, $.string, $.block),
+    _value: $ => choice($.bool, $.number, $.string, $.named_block, $.block),
   }
 })
